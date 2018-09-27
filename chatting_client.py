7e9853ecf696d4c7d71b362c7b2316f622cc5112
daegeun <lollm@naver.com>
2018-09-27 10:50:58 +0900

    update : chatting_server.py - add get_shape_and_size() func

--- chatting_client.py
+++ chatting_client.py
@@ -11,13 +11,17 @@ s.connect((HOST, PORT))
 
 def sendingVideo():
     capture = cv2.VideoCapture(0)
+    #이미지 크기 전송
+    _ret, _frame = capture.read()
+    s.send(str(_frame.shape).encode())
+    #영상 프레임 단위로 전송
     while True:
         ret, frame = capture.read()
         numpy_to_bytes = frame.tobytes()
-        #s.send(str(len(stringData)).ljust(16).encode())
         s.send(numpy_to_bytes)
     s.close()
 
+    #영상 끝나면 끝내기
     capture.release()
     cv2.destroyAllWindows()
 
--- chatting_server.py
+++ chatting_server.py
@@ -12,9 +12,12 @@ s.listen(1)
 conn, addr = s.accept()
 print('Connected by', addr)
 
+#buffer에 나눠서 담기는 것들을 전부 하나로 모아서 가져오도록 한다.
 def recv_all(count):
     buf = b''
     while count:
+        #버퍼 하나의 사이즈는 반드시 count/(2^n)인 값이 와야 한다.
+        #물론 버퍼 하나의 사이즈가 count 자체이면 제일 빠르고 좋다.
         newbuf = conn.recv(count)
         if not newbuf:
             return None
@@ -22,11 +25,31 @@ def recv_all(count):
         count -= len(newbuf)
     return buf
 
+#영상의 shape과 size를 반환
+def get_shape_and_size(s):
+    shape = ()
+    start=1
+    end=0
+    for i in s:
+        if i==',' or i==')':
+            tmp_tup = (int(s[start:end]),)
+            shape+=tmp_tup
+            start=end+2
+        end+=1
+    size = 1
+    for i in shape:
+        size*=i
+    return shape, size
+
+#영상을 받는 함수
 def gettingVideo():
+    data = conn.recv(1024)
+    data = data.decode()
+    shape, size = get_shape_and_size(data)
     while True:
-        data = recv_all(921600) #921600 = 480*640*3
-        byte_to_numpy = numpy.frombuffer(data, numpy.uint8).reshape(480,640,3)
-        cv2.imshow('hi',byte_to_numpy)
+        data = recv_all(size) #921600 = 480*640*3
+        byte_to_numpy = numpy.frombuffer(data, numpy.uint8).reshape(shape)
+        cv2.imshow('client to server',byte_to_numpy)
         if cv2.waitKey(1) == 27:
             break
     cv2.destroyAllWindows()
